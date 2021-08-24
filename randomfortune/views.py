b4106e4bd5b2c7bfa07aaccb6b83788b11b10e4d
rebecca-agyei <rebecca.agyei@amalitech.org>
2021-08-24 15:36:22 +0000

    Commit of my project

--- randomfortune/views.py
+++ randomfortune/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
+
+fortuneList = [
+   "You have a bright future",
+   "Be content with what you have",
+   "Knowledge is power",
+   "Happy yourself",
+   "You can do it"
+]
 def fortune(request):
-  return render(request, "randomfortune/fortune.html")
+    fortune = random.choice(fortuneList)
+    context = {
+        'fortune': fortune
+    }
+    return render(request, "randomfortune/fortune.html", context)
